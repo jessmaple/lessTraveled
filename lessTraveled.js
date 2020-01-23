20a3aa8e5b980b040f97564f7d67be84e72804ea
Alan Margolies <amargolies88@gmail.com>
2020-01-23 15:55:34 -0500

    all endpoint responses log to console for selected state

--- lessTraveled.js
+++ lessTraveled.js
@@ -70,6 +70,8 @@ var npsApiURL = "https://developer.nps.gov/api/v1/";
 
 //Create array of endpoints
 var npsEndpointArray = [
+  "alerts?",
+  "articles?",
   "campgrounds?",
   "events?",
   "lessonplans?",
@@ -87,26 +89,99 @@ var stateSelect = $("#states");
 function appendStates() {
   for (let i = 0; i < stateNames.length; i++) {
     const element = stateNames[i];
-    stateSelect.append($("<option>").val(stateNames[i]).text(stateNames[i]));
+    var stateOption = $("<option>").val(stateNames[i]).text(stateNames[i]);
+    stateOption.click(function (event) {
+
+    });
+
+    stateSelect.append(stateOption);
+
   }
 }
 
 appendStates()
-$("#states").on("change", function() {
-  $("label.btn")[0].click();
+
+$("#states").on("change", function () {
+  console.log($(this).val());
+
+  //Set state variable to user-selected state
+  var state = stateKeys[$(this).val()];
+  //API query
+  var query = "stateCode=";
+  //Set seperate spans for static text Loading and animated dots...
+  var loadSpan = $("<span>").text("Loading");
+  var dotSpan = $("<span>").text(".");
+
+  //Counting variable for the following interval. (Probably could be used in a cleaner way)
+  var stupidCount = 0;
+  //Interval for animating dots
+  var loadingAnimation = setInterval(function () {
+    //Once count reaches 11
+    if (stupidCount > 10) {
+      //Clear the dots and restart stupidCount
+      dotSpan.empty();
+      stupidCount = 0;
+    }
+    //Continue adding dots
+    dotSpan.text(dotSpan.text() + ".");
+    //Incriment stupidCount
+    stupidCount++;
+  }, 250);
+
+  var responseArray = [];
+
+
+  // Recursive API call
+  // could be dangerous to break this. an infinite loop might
+  // cause the api key to exceed its usage limit
+  // monitor each recursion through console
+  giveEverything();
+  function giveEverything() {
+    var curEndPoint = npsEndpointArray.shift();
+    $.ajax({
+      url: npsApiURL + curEndPoint + query + state + npsApiKey,
+      method: "GET"
+    }).then(function (response) {
+      console.log("loop " + stupidCount);
+      //store response in responsearray
+      responseArray.push({
+        endPoint: curEndPoint,
+        res: response
+      });
+      console.log("stored response from endpoint: " + curEndPoint);
+      //Stop the animation interval and clear its elements display response
+      //if finished
+      if (npsEndpointArray.length < 1) {
+        clearInterval(loadingAnimation);
+        //Logging response
+        for (let i = 0; i < responseArray.length; i++) {
+          console.log("--------------------------------")
+          console.log("endpoint: " + responseArray[i].endPoint);
+          console.log("response: ");
+          console.log(responseArray[i].res);
+        }
+        //DISPLAY MODAL
+        $("label.btn")[0].click();
+      } else {
+        giveEverything();
+      }
+    });
+  }
 });
 
 // Map JS
 
 function initMap() {
-  var Denali = {lat:63.734353 , lng:-148.912016 };
-  
+  var Denali = { lat: 63.734353, lng: -148.912016 };
+
   var map = new google.maps.Map(
     document.getElementById('map'), {
-      zoom: 4, center: Denali});
+    zoom: 4, center: Denali
+  });
 
-  var marker = new google.maps.Marker ({
-    position: Denali, map: map})
-  }
+  var marker = new google.maps.Marker({
+    position: Denali, map: map
+  })
+}
 
 
